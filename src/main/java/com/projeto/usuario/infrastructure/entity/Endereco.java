664475274da181d919abedc8e650dab8fa713945
biadjordjevic <biadjordjevic@gmail.com>
2026-03-22 14:57:02 -0300

    adicionadas classes de security

---
+++ src/main/java/com/projeto/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,33 @@
+package com.projeto.usuario.infrastructure.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(name = "rua")
+    private String Rua;
+    @Column(name = "numero")
+    private Long numero;
+    @Column(name = "complemento", length = 10)
+    private String complemento;
+    @Column(name = "cidade", length = 150)
+    private String cidade;
+    @Column(name = "estado", length = 2)
+    private String estado;
+    @Column(name = "cep", length = 9)
+    private String cep;
+
+
+
+}
